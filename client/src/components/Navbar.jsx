4e988fe295eee454a00850ef52f08bc70dfdbd56
Prabhjot Singh <prabhjots5498@gmail.com>
2024-07-17 15:27:21 +0530

    Created Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import explore from "../assets/explore.svg";
+import profile from "../assets/profile.svg";
+import search from "../assets/search.svg";
+import "../styles/Navbar.css";
+import "../styles/index.css";
+
+function Navbar() {
+    return (
+        <div className="navbar div-padding ">
+            <p className="website-name dm-serif-display-regular">SocietySphere</p>
+            <div className="navbar-routes">
+                <div className="navbar-route">
+                    <img src={explore} alt="" className="navbar-route-logo"/>
+                    <p className="navbar-route-name inter">Explore</p>
+                </div>
+                <div className="navbar-route">
+                    <img src={search} alt="" className="navbar-route-logo"/>
+                    <p className="navbar-route-name inter">Search</p>
+                </div>
+            </div>
+            <img src={profile} alt="" className="navbar-route-logo profile"/>
+        </div>
+    )
+}
+
+export default Navbar;
