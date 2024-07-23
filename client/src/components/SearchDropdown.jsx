2004013dca07f35ec545c9f412c6cbf1b4b1bf71
Prabhjot Singh <prabhjots5498@gmail.com>
2024-07-24 00:24:15 +0530

    updated dropdown search

--- client/src/components/SearchDropdown.jsx
+++ client/src/components/SearchDropdown.jsx
@@ -10,32 +10,58 @@ function SearchDropdown({display}) {
     const [colleges, setColleges] = useState([]);
     const [societies, setSocieties] = useState([]);
 
-    function handleCities(id) {
+    const [selectedState, setSelectedState] = useState("");
+    const [selectedCity, setSelectedCity] = useState("");
+    const [selectedCollege, setSelectedCollege] = useState("");
+    const [selectedSociety, setSelectedSociety] = useState("");
+
+    const [searchURL, setSearchURL] = useState("");
+
+    function handleCities(item) {
         setCities(() => {
-            const newCities = [...dummyData.cities].filter(item => item.state_id === id);
+            const newCities = [...dummyData.cities].filter(i => i.state_id === item.id);
             return newCities;
         });
         setColleges(() => {
             return [];
-        })
+        });
+        setSocieties(() => {
+            return [];
+        });
+        setSelectedState(item.name);
+        setSelectedCity("");
+        setSelectedCollege("");
+        setSelectedSociety("");
     }
 
-    function handleColleges(id) {
+    function handleColleges(item) {
         setColleges(() => {
-            const newColleges = [...dummyData.colleges].filter(item => item.city_id === id);
+            const newColleges = [...dummyData.colleges].filter(i => i.city_id === item.id);
             return newColleges;
         });
         setSocieties(() => {
             return [];
-        })
+        });
+        setSelectedCity(item.name);
+        setSelectedCollege("");
+        setSelectedSociety("");
     }
 
-    function handleSocieties(id) {
+    function handleSocieties(item) {
         setSocieties(() => {
-            const newSocieties = [...dummyData.societies].filter(item => item.college_id === id);
+            const newSocieties = [...dummyData.societies].filter(i => i.college_id === item.id);
             return newSocieties;
         });
+        setSelectedCollege(item.name);
+        setSelectedSociety("");
     }
+
+    function handleQuery(item) {
+        setSelectedSociety(item.name);
+        // setSearchURL(() => `http://localhost:5173/search?state=${selectedState}&city=${selectedCity}&college=${selectedCollege}&society=${selectedSociety}`);
+    }
+
+    // console.log(searchURL);
 
     return (
         <div className="search-dropdown-container" style={{display: display}}>
@@ -45,10 +71,10 @@ function SearchDropdown({display}) {
                 </div>
                 <div className="state-names dropdown-values">
                     {states.map(item => {
-                        return <div className="state-name inter dropdown-value" key={item.id} onMouseEnter={() => handleCities(item.id)}>{item.name}</div>
+                        return <div className={`state-name inter dropdown-value ${selectedState === item.name && 'selected-item'}`} key={item.id} onClick={() => handleCities(item)}>{item.name}</div>
                     })}
                 </div>
-                {states.length > 5 && <p className="see-more inter">See More</p>}
+                {/* {states.length > 5 && <p className="see-more inter">See More</p>} */}
             </div>
             <div className="cities search-dropdown">
                 <div className="city dropdown-headline">
@@ -56,10 +82,9 @@ function SearchDropdown({display}) {
                 </div>
                 <div className="city-names dropdown-values">
                     {cities.map(item => {
-                        return <div className="city-name inter dropdown-value" key={item.id} onMouseEnter={() => handleColleges(item.id)}>{item.name}</div>
+                        return <div className={`city-name inter dropdown-value ${selectedCity === item.name && 'selected-item'}`} key={item.id} onClick={() => handleColleges(item)}>{item.name}</div>
                     })}
                 </div>
-                {cities.length > 5 && <p className="see-more inter">See More</p>}
             </div>
             <div className="colleges search-dropdown">
                 <div className="college dropdown-headline">
@@ -67,10 +92,9 @@ function SearchDropdown({display}) {
                 </div>
                 <div className="college-names dropdown-values">
                     {colleges.map(item => {
-                        return <div className="college-name inter dropdown-value" key={item.id} onMouseEnter={() => handleSocieties(item.id)}>{item.name}</div>
+                        return <div className={`college-name inter dropdown-value ${selectedCollege === item.name && 'selected-item'}`} key={item.id} onClick={() => handleSocieties(item)}>{item.name}</div>
                     })}
                 </div>
-                {colleges.length > 5 && <p className="see-more inter">See More</p>}
             </div>
             <div className="societies search-dropdown">
                 <div className="society dropdown-headline">
@@ -78,7 +102,7 @@ function SearchDropdown({display}) {
                 </div>
                 <div className="society-names dropdown-values">
                     {societies.map(item => {
-                        return <div className="society-name inter dropdown-value" key={item.id}>
+                        return <div className={`society-name inter dropdown-value ${selectedSociety === item.name && 'selected-item'}`} key={item.id} onClick={() => handleQuery(item)}>
                             <p>{item.name}</p>
                             <Tag tag={item.type} color={colorMapping[[item.type]]} />
                             </div>
