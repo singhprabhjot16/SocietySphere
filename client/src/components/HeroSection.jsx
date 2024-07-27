1b7695c8dd95a56a0e9e2c11cc1e8dd30477438e
Prabhjot Singh <prabhjots5498@gmail.com>
2024-07-27 20:23:41 +0530

    Hero Section and Footer Updates

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,10 +1,15 @@
 import "../styles/HeroSection.css"
 import "../styles/index.css"
-import "../assets/group-1.svg"
+import heroSectionImage from "../assets/group-1.svg"
+
 function HeroSection() {
     return (
-        <div className="hero-section">
-            <div className="website"></div>
+        <div className="hero-section div-padding">
+            <div className="website">
+                <p className="website-name-hero dm-serif-display-regular">SocietySphere</p>
+                <p className="website-tagline poppins-regular">Discover. Connect. Grow</p>
+            </div>
+            <img src={heroSectionImage} alt=""/>
         </div>
     );
 }
---
+++ client/src/components/QueryForm.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "../styles/index.css";
+import "../styles/QueryForm.css";
+
+function QueryForm() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        query: ""
+    });
+
+    function handleChange(event) {
+        setFormData((prevData) => {
+            const name = event.target.name;
+            const value = event.target.value;
+            return {...prevData, [name]: value};
+        });
+    }
+
+    return (
+        <div className="query-form-container div-padding">
+            <form action="POST" className="query-form">
+                <p className="form-headline poppins-medium">Have a question? Feel free to ask
+                and we will get back to you.</p>
+                <input type="text" onChange={handleChange} value={formData.name} name="name" placeholder="Name" className="poppins-regular input-field"/>
+                <input type="email" onChange={handleChange} value={formData.email} name="email" placeholder="Email" className="poppins-regular input-field"/>
+                <textarea type="" onChange={handleChange} value={formData.query} name="query" placeholder="Query" className="poppins-regular input-field"/>
+                <button type="submit" className="submit-button poppins-regular">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default QueryForm;
