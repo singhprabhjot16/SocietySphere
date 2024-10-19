6766303ea34b7a83225bde5506bafda17ffd3c6a
Prabhjot Singh <prabhjots5498@gmail.com>
2024-10-19 17:52:21 +0530

    Updated PORT

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import searchRoutes from './routes/search.js';
 import societiesInfo from './routes/society.js';
+import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 6001;
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/search', searchRoutes);
 app.use('/society', societiesInfo);
 
