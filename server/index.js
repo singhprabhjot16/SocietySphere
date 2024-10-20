54c43d8aafac8b7012271aceea6d6ba062e45158
unknown <prabal.gupta2003@gmail.com>
2024-10-20 14:39:34 +0530

    Port changed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import searchRoutes from './routes/search.js';
 import societiesInfo from './routes/society.js';
-
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/search', searchRoutes);
